e43641fdcbde14a761fb2c8e65d7d16260af7dba
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-11-05 16:48:43 -0400

    adding new route for new npc

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :npcs
+  get 'new', to: "np_cs#index"
   get 'wakeup', to: 'np_cs#wakeup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

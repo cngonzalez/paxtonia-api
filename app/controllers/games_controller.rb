a0c64453f818579d828a20e9d0e8aecb374903e4
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-11-08 16:30:13 -0500

    adding new game method to controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,10 @@
 class GamesController < ApplicationController
+  
+  def new
+    game = Game.new(hero_reputation: rand(100))
+    game.npcs.build(name: Faker::Name.name, personality: rand(100))
+    if game.save
+      render json: game.to_json(:include => :npcs)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :npcs do 
-    resources :responses, only: [:new]
-  end
-  
+  get 'newgame', to: 'games#new' 
   get 'new', to: "np_cs#index"
   get 'wakeup', to: 'np_cs#wakeup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

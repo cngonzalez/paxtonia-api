dfb8f00cd5ba5c9ab9022e860b6f4689379866b8
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-11-10 09:11:29 -0500

    added route for getting responses

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,4 +7,21 @@ class GamesController < ApplicationController
       render json: game.to_json(:include => :npcs)
     end
   end
+
+  def talk
+    npc = Game.find(params[:id]).npcs.last
+    npc.responses.build(input: talk_params[:input])
+    if npc.save
+      render json: npc.responses.last.to_json
+    else 
+      render json: npc.errors.to_json
+    end
+  end
+
+  private
+  
+  def talk_params
+    params.require(:talk).permit(:input)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'newgame', to: 'games#new' 
-  get 'new', to: "np_cs#index"
+  post '/games/:id/talk', to: 'games#talk'
   get 'wakeup', to: 'np_cs#wakeup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
